14127e6588851e37fdda2ee2a0b90a8765d3fae9
JackyChuang <chchuang640509@gmail.com>
2021-06-08 16:03:09 +0800

    Add Math Branch

--- GitBranchSampleApp/src/GitBranchApp/mainApp.java
+++ GitBranchSampleApp/src/GitBranchApp/mainApp.java
@@ -6,6 +6,14 @@ public class mainApp {
 		// TODO Auto-generated method stub
 		// 這是第一個版本
 		System.out.println("1st Version");
+		Math cMath = new Math();
+		System.out.println();
+		System.out.println(cMath.add(10, 20));
 	}
 
+}
+class Math{
+	int add(int a, int b) {
+		return a+b;
+	}
 }
